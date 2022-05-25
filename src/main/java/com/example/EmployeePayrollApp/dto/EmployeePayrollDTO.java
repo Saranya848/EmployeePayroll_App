3bbe37d01697961d9c88d46cd362ea1d9ef292b1
SaranyaSiripurapu <saranyasripurapu@gmail.com>
2022-05-25 21:45:05 +0530

    Saving employee data in database

--- src/main/java/com/example/EmployeePayrollApp/dto/EmployeePayrollDTO.java
+++ src/main/java/com/example/EmployeePayrollApp/dto/EmployeePayrollDTO.java
@@ -1,26 +1,40 @@
 package com.example.employeepayrollapp.dto;
 
-import javax.validation.constraints.Min;
-import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.Pattern;
+//import com.fasterxml.jackson.annotation.JsonFormat;
+//import lombok.ToString;
+//
+//import javax.validation.constraints.Min;
+//import javax.validation.constraints.NotEmpty;
+//import javax.validation.constraints.Pattern;
+//import java.time.LocalDate;
+//import java.util.List;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.ToString;
 
-public class EmployeePayrollDTO {
-
-
+import javax.validation.constraints.*;
+import java.time.LocalDate;
+import java.util.List;
+public  @ToString class  EmployeePayrollDTO {
     @NotEmpty(message = "Employee name cannot be null")
     @Pattern(regexp = "^[A-Z]{1}[a-zA-Z\\s]{2,}$", message = "Employee name is INVALID")
     public String name;
+
     @Min(value = 500, message = "Min wage should be more than 500")
     public long salary;
+    @Pattern(regexp = "male|female", message = " Gender  needs to be male or female")
+    public String gender;
 
-    public EmployeePayrollDTO(String name, long salary) {
-        this.name = name;
-        this.salary = salary;
-    }
+    @JsonFormat(pattern = "dd MMM yyyy")
+    @NotNull(message = "Start date should not be empty")
+    @PastOrPresent(message = "startDate should be past or todays date")
+    public LocalDate startDate;
 
-    @Override
-    public String toString() {
-        return "name='" + name + '\'' +
-                ", salary=" + salary ;
-    }
+    @NotBlank(message = "Note can not be empty")
+    public String note;
+
+    @NotBlank(message = "profilePic can not be empty")
+    public String profilePic;
+
+    @NotNull(message = "department should not be empty")
+    public List<String> departments;
 }
---
+++ src/main/java/com/example/EmployeePayrollApp/exceptions/EmployeePayrollExceptionHandler.java
@@ -0,0 +1,41 @@
+package com.example.employeepayrollapp.exceptions;
+
+import com.example.employeepayrollapp.dto.ResponseDTO;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@ControllerAdvice
+@Slf4j
+public class EmployeePayrollExceptionHandler {
+    private static final String message = "Exception While Processing REST Request";
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<ResponseDTO> handelHttpMessageNotReadableException(
+            HttpMessageNotReadableException exception) {
+        log.error("Invalid Date Format", exception);
+        ResponseDTO respDTO = new ResponseDTO(message, "Should have date in dd MMM yyyy format");
+        return new ResponseEntity<ResponseDTO>(respDTO, HttpStatus.BAD_REQUEST);
+    }
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ResponseDTO> handleMethodArgumentNotValidException(MethodArgumentNotValidException exception) {
+        List<ObjectError> errorList = exception.getBindingResult().getAllErrors();
+        List<String> errorMessage = errorList.stream()
+                .map(objectError -> objectError.getDefaultMessage())
+                .collect(Collectors.toList());
+        ResponseDTO responseDTO = new ResponseDTO("Exception while processing REST request", errorMessage);
+        return new ResponseEntity<ResponseDTO>(responseDTO, HttpStatus.BAD_REQUEST);
+    }
+    @ExceptionHandler(EmployeePayrollCustomException.class)
+    public ResponseEntity<ResponseDTO> handleEmployeePayrollCustomException(EmployeePayrollCustomException exception) {
+        ResponseDTO responseDTO = new ResponseDTO("Exception while processing REST request", exception.getMessage());
+        return new ResponseEntity<ResponseDTO>(responseDTO, HttpStatus.BAD_REQUEST);
+    }
+}
